fe94215e6f8f9842e520fb8a5e389357c067b25a
thaigialai1987 <thaigialai1987@gmail.com>
2015-08-04 17:18:47 +0700

    -first commit

---
+++ mvcmusicstore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcmusicstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Here there.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact this.";
+
+            return View();
+        }
+    }
+}
